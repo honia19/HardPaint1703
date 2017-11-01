7b8aa638058199d9407058a238ff1cb5987add18
honia19 <honchenko@gmail.com>
2017-11-02 00:31:09 +0200

    skin done

--- app/src/main/java/com/example/vladislav/painterdemo/MainActivity.java
+++ app/src/main/java/com/example/vladislav/painterdemo/MainActivity.java
@@ -2,15 +2,11 @@ package com.example.vladislav.painterdemo;
 
 import android.content.Intent;
 import android.content.res.Configuration;
-import android.support.v4.app.NavUtils;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-import android.support.v7.widget.Toolbar;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.Toast;
 
 import java.util.Locale;
